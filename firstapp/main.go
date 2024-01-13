9e738d376e9c20996aaa7fe5047d339d5f1dc2b6
zewenw <wangzewen8527@163.com>
2024-01-13 18:40:36 +0000

    variabale definition

--- firstapp/main.go
+++ firstapp/main.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+var j float32 = 6.0
+
+var (
+	actorName string = "Elon"
+	companion string = "sarah"
+	doctorNumber int = 3
+	season = 11
+)
+
+
 func main(){
-	fmt.Println("hello world")
+	i := 42
+	fmt.Println(i)
+	fmt.Printf("%v, %T", i , i)
+	fmt.Printf("%v, %T", j , j)
 }
